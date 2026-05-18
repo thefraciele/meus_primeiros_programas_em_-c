647798978dce3db5054f10ea906e92e07f339352
Franciele <francieleduraes22@gmail.com>
2026-05-18 20:18:53 -0300

    estudei entrada e saida de dados
    entrada e saída de dados

---
+++ tema2/saida_de_dados.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main () {
+    int idade=20;//int significa inteiro
+    float altura =1.63;
+    char nome[20] = "franciele";
+
+    printf("A idade da %s é: %d\n", nome, idade);
+
+    return 0;
+
+
+}
