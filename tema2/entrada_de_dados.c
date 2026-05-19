f2c93cbcc2eb037a62543081e0ccc3e8eba693a7
Franciele <francieleduraes22@gmail.com>
2026-05-19 14:34:57 -0300

    adicionei a variável altura
    adicionei a variável altura, adicionei a função printf e scanf para pegar a informação e mostrar na tela

--- tema2/entrada_de_dados.c
+++ tema2/entrada_de_dados.c
@@ -1,6 +1,7 @@
 #include <stdio.h> 
 int main () {
     int idade; //aqui eu defino o tipo da minha variavel que é um número inteiro
+    float altura;
 
     printf("Digite sua idade:\n"); 
     // aqui não apenas mostramos algo na tela (eu não preciso colocar um especificador de formato aqui porque ele funciona como um placeholder)
@@ -8,9 +9,12 @@ int main () {
     scanf("%d", &idade);
     // pegue o que foi digitado na tela e salve na memória 
       
-
-    printf("Sua idade é: %d", idade);
+    printf("Sua idade é: %d\n", idade);
     //printf("%d", idade); o %d significa (pegue o número da memoria e mostre na tela)
+
+    printf("digite sua altura:\n");
+    scanf("%f", &altura);
+    printf("Sua altura é: %.2f", altura);
 
     return 0;
 }
