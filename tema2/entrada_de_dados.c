cd8e7476753b484382bc923f62edefb8e15ac691
Franciele <francieleduraes22@gmail.com>
2026-05-19 16:36:26 -0300

    variável "letra"
    adicionei a variável "letra" e usei as funções printf e scanf para interação

--- tema2/entrada_de_dados.c
+++ tema2/entrada_de_dados.c
@@ -3,6 +3,7 @@ int main () {
     int idade; //aqui eu defino o tipo da minha variavel que é um número inteiro
     float altura;
     char nome[20];
+    char letra;
 
     printf("Digite sua idade:\n"); 
     // aqui não apenas mostramos algo na tela (eu não preciso colocar um especificador de formato aqui porque ele funciona como um placeholder)
@@ -19,8 +20,14 @@ int main () {
 
     printf("Digite apenas seu primeiro nome:\n");
     scanf("%s", nome);
-    printf("Seu nome é: %s", nome);
+    printf("Seu nome é: %s\n", nome);
     //a linguagem #C indentifica o espaço como uma finalização assim como enter então dessa forma não da pra fazer um nome composto
+
+    printf("Digite sua letra favorita:");
+    scanf(" %c", &letra);
+    printf("Sua letra favorita é:%c\n", letra);
+
+
 
     return 0;
 }
