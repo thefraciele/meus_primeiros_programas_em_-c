8e53428b197985b5cfdf6539bd88a7499fd12175
Franciele <francieleduraes22@gmail.com>
2026-05-23 19:01:18 -0300

    estudo de incrementos
    começando um programa de incremento e decremento de valores

---
+++ tema2/operadores_incremento.c
@@ -0,0 +1,12 @@
+include <stdio.h>
+int main (){
+    /*
+    incremento (++)
+    pré-incremento ++a
+    pós-incremento a++
+    decremento (--)
+    pré-decremento --a
+    pós-decremento a--
+    */
+
+}
